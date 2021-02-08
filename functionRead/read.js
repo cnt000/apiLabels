67e90bd833cb1b6a0b0200bedb694251be37ef29
Edoardo Gargano <edoardo.gargano@monrif.net>
2021-02-08 19:23:53 +0100

    return Item prop from read

--- functionRead/read.js
+++ functionRead/read.js
@@ -19,7 +19,7 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: data,
+        message: data.Item,
       }),
     };
   } catch (err) {
